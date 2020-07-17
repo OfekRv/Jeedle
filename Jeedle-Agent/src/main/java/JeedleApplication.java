2a05c581ab6593cad8395b7642e27a36e94bd371
ofekrv <ofekrv@gmail.com>
2020-07-17 22:50:01 +0300

    modules

---
+++ Jeedle-Agent/src/main/java/JeedleApplication.java
@@ -0,0 +1,9 @@
+import java.lang.instrument.Instrumentation;
+
+public class JeedleApplication {
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+    }
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+    }
+}
---
+++ Jeedle-Dropper/src/main/java/DropperApplication.java
@@ -0,0 +1,6 @@
+public class DropperApplication {
+    public static void main(String args[])
+    {
+        System.out.println("init");
+    }
+}
