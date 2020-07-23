5b51852040aa8219ad1c2bddb4d807d028c071f1
ofekrv <ofekrv@gmail.com>
2020-07-23 20:50:18 +0300

    project fix

--- Jeedle-Agent/src/main/java/JeedleApplication.java
+++ Jeedle-Agent/src/main/java/JeedleApplication.java
@@ -2,6 +2,10 @@ import java.lang.instrument.Instrumentation;
 
 public class JeedleApplication {
     public static void agentmain(String agentArgs, Instrumentation inst) {
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+        }
     }
 
     public static void premain(String agentArgs, Instrumentation inst) {
